1856de43b4b0d6f636709c8234a3afeb304e9a54
vatsal <vatsaldubeywk@gmail.com>
2026-04-20 09:07:33 +0530

    new

--- experiment-6.js
+++ experiment-6.js
@@ -4,26 +4,22 @@ let input = document.getElementById("userInput");
 
 let fontSize = 16;
 
-// Change heading text (onclick using addEventListener)
 document.getElementById("changeTextBtn").addEventListener("click", function () {
     if (input.value !== "") {
         heading.innerHTML = input.value;
     }
 });
 
-// Change background color
 document.getElementById("bgColorBtn").onclick = function () {
     document.body.style.backgroundColor =
         "#" + Math.floor(Math.random() * 16777215).toString(16);
 };
 
-// Increase font size
 document.getElementById("fontSizeBtn").addEventListener("click", function () {
     fontSize += 2;
     paragraph.style.fontSize = fontSize + "px";
 });
 
-// Show/Hide paragraph
 document.getElementById("toggleBtn").addEventListener("click", function () {
     if (paragraph.style.display === "none") {
         paragraph.style.display = "block";
@@ -32,7 +28,6 @@ document.getElementById("toggleBtn").addEventListener("click", function () {
     }
 });
 
-// Reset page
 document.getElementById("resetBtn").addEventListener("click", function () {
     heading.innerHTML = "Welcome to JavaScript Lab";
     paragraph.style.display = "block";
