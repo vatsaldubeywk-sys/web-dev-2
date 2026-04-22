e9548e28146688f447106400bc4c585d57dbb1b2
vatsaldubey55-hub <vatsaldubey55@gmail.com>
2026-04-22 21:04:33 +0530

    x

--- hello/src/main.jsx
+++ hello/src/main.jsx
@@ -1,24 +1,39 @@
-function StudentList (){
-    const students = ["Rahul","Sahul","Mahul"]
-    return (
-        <ul>
-        {students.map((s,index)=>(
-            <li key={index}>{s}</li>
-        ))}
-        </ul>
-    )
+function handleSubmit() {
+  let name = document.getElementById("name");
+  let roll = document.getElementById("roll");
+  let email = document.getElementById("email");
+  let phone = document.getElementById("phone");
+
+  if (name === "") {
+    alert("Student Name is required");
+    return;
+  }
+
+  if (roll === "") {
+    alert("Roll Number is required");
+    return;
+  }
+
+  if (email === "") {
+    alert("Email is required");
+    return;
+  }
+
+  if (!email.includes("@")) {
+    alert("@ missing");
+    return;
+  }
+
+  if (phone === "") {
+    alert("Phone Number is required");
+    return;
+  }
+
+  if (phone.length!=10) {
+    alert("Phone Number exactly 10 digits ka hona chahiye");
+    return;
+  }
+
+  alert("Form submitted successfully");
 }
-
-
-
-/* import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-*/
+console.log("hello");
