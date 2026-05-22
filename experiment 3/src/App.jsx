42734f24ed23e59d15e18e4544eb3e6162efee99
vatsaldubey55-hub <vatsaldubey55@gmail.com>
2026-05-22 16:11:54 +0530

    hello

---
+++ experiment 3/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar } from './navbar'
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
---
+++ experiment 3/src/navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center gap-4">
+        <div>
+          <h2>TEMU</h2>
+        </div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Share</li>
+            <li>About</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
