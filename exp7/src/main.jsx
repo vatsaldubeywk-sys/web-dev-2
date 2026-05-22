d2ac70e02751aebb6d80ea36816714e78dd83ae4
vatsaldubey55-hub <vatsaldubey55@gmail.com>
2026-05-22 16:18:58 +0530

    m

--- exp7/src/main.jsx
+++ exp7/src/main.jsx
@@ -27,13 +27,3 @@ function Student(props) {
 
 
 
-/* import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)*/
--- exp8/src/main.jsx
+++ exp8/src/main.jsx
@@ -16,13 +16,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<CounterApp />);
 
 
-/*import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)*/
